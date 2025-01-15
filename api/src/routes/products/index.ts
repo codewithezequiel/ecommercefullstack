5f1fd3e220bbffabbfe582f2283bb42d6f571ad1
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2025-01-14 17:45:06 -0800

    Extract controller from product router boilerplate.

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -1,32 +1,18 @@
 import { Router } from "express";
+import {
+  createProduct,
+  deleteProduct,
+  getProductById,
+  listProducts,
+  updateProduct,
+} from "./productsController";
 
-// Products Endpoints
 const router = Router();
-// Endpoint 1: Getting all products
-router.get("/", (req, res) => {
-  res.send("Welcome to the products page!");
-});
 
-// Endpoint 2: Getting a single product by id
-router.get("/:id", (req, res) => {
-  console.log(req.params);
-  res.send("productById");
-});
-
-// Endpoint 3: Adding a product
-router.post("/", (req, res) => {
-  console.log(req.params);
-  res.send("creatingAProduct");
-});
-
-// Endpoint 4: Modifying a product
-router.put("/:id", (req, res) => {
-  res.send("updatingProduct");
-});
-
-// Endpoint 5: Deleting a product
-router.delete("/:id", (req, res) => {
-  res.send("deletingProduct");
-});
+router.get("/", listProducts);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 export default router;
---
+++ api/src/routes/products/productsController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export function listProducts(req: Request, res: Response) {
+  res.send("Welcome to the products page!");
+}
+
+export function createProduct(req: Request, res: Response) {
+  res.send(" creating a product");
+}
+
+export function getProductById(req: Request, res: Response) {
+  res.send("productById");
+}
+
+export function updateProduct(req: Request, res: Response) {
+  res.send("updating Product");
+}
+
+export function deleteProduct(req: Request, res: Response) {
+  res.send("Deleting product");
+}
