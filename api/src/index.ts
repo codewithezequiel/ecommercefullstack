f315adf863299ede016927eb2d816d9dc49d548b
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2025-01-14 17:50:06 -0800

    json parsing middleware

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,9 @@
-import express from "express";
+import express, { json, urlencoded } from "express";
 import productsRouter from "./routes/products/index";
 
 const app = express();
+app.use(json());
+app.use(urlencoded({ extended: false }));
 const port = 3000;
 
 app.get("/", (req, res) => {
