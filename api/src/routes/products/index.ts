e19a1952fee11ea57b34db6ec7a04a15e8895210
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2025-01-16 04:28:28 -0800

    Fix build distribution from typescript to javascript.

--- api/src/routes/products/index.ts
+++ api/src/routes/products/index.ts
@@ -5,13 +5,13 @@ import {
   getProductById,
   listProducts,
   updateProduct,
-} from "./productsController";
-import { validateData } from "../../middlewares/validationMiddleware";
+} from "./productsController.js";
+import { validateData } from "../../middlewares/validationMiddleware.js";
 import {
   createProductSchema,
   updateProductSchema,
-} from "../../db/productsSchema";
-import { verifySeller, verifyToken } from "../../middlewares/authMiddleware";
+} from "../../db/productsSchema.js";
+import { verifySeller, verifyToken } from "../../middlewares/authMiddleware.js";
 
 const router = Router();
 
--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
-import { db } from "../../db/index";
-import { productsTable, createProductSchema } from "../../db/productsSchema";
+import { db } from "../../db/index.js";
+import { productsTable } from "../../db/productsSchema.js";
 import { eq } from "drizzle-orm";
 import _ from "lodash";
 
@@ -9,6 +9,7 @@ export async function listProducts(req: Request, res: Response) {
     const products = await db.select().from(productsTable);
     res.json(products);
   } catch (error) {
+    console.log(error);
     res.status(500).send(error);
   }
 }
