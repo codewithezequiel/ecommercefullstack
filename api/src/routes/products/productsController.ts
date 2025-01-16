7426eea840997f377bf01b92dcb01fb0da19336e
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2025-01-15 16:03:19 -0800

    Products CRUD operations.

--- api/src/routes/products/productsController.ts
+++ api/src/routes/products/productsController.ts
@@ -1,22 +1,83 @@
 import { Request, Response } from "express";
+import { db } from "../../db/index";
+import { productsTable } from "../../db/productsSchema";
+import { eq } from "drizzle-orm";
 
-export function listProducts(req: Request, res: Response) {
-  res.send("Welcome to the products page!");
+export async function listProducts(req: Request, res: Response) {
+  try {
+    const products = await db.select().from(productsTable);
+    res.json(products);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
-export function createProduct(req: Request, res: Response) {
-  console.log(req.body);
-  res.send(" creating a product");
+export async function createProduct(req: Request, res: Response) {
+  try {
+    const [product] = await db
+      .insert(productsTable)
+      .values(req.body)
+      .returning();
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
-export function getProductById(req: Request, res: Response) {
-  res.send("productById");
+export async function getProductById(req: Request, res: Response) {
+  try {
+    const { id } = req.params;
+    const [product] = await db
+      .select()
+      .from(productsTable)
+      .where(eq(productsTable.id, Number(id)));
+
+    if (!product) {
+      res.status(404).send("Product not found");
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
-export function updateProduct(req: Request, res: Response) {
-  res.send("updating Product");
+export async function updateProduct(req: Request, res: Response) {
+  try {
+    const id = Number(req.params.id);
+    const updatedFields = req.body;
+    const [updatedProduct] = await db
+      .update(productsTable)
+      .set(updatedFields)
+      .where(eq(productsTable.id, id))
+      .returning();
+
+    if (!updatedProduct) {
+      res
+        .status(400)
+        .send({ message: "Product with specified id was not found" });
+    } else {
+      res.json(updatedProduct);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
-export function deleteProduct(req: Request, res: Response) {
-  res.send("Deleting product");
+export async function deleteProduct(req: Request, res: Response) {
+  try {
+    const id = Number(req.params.id);
+    const [deletedProduct] = await db
+      .delete(productsTable)
+      .where(eq(productsTable.id, id))
+      .returning();
+
+    if (deletedProduct) {
+      res.status(204).send();
+    } else {
+      res.status(404).send({ message: "Product was not found" });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
